55ec3b82605c7e6a3f5b9b9a15564e98f987f6a8
包涵 <1004578954@qq.com>
2020-07-14 14:39:51 +0800

    first commit

---
+++ Assets/Scripts/Test2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    delegate void NumberChanger(int num);
+    public int a=0;
+    NumberChanger nc;
+    public void MultNum(int num)
+    {
+        a *= num;
+        Debug.Log(a);
+    }
+    public void AddNum(int num)
+    {
+        a += num;
+        Debug.Log(a);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            nc = delegate (int num)
+            {
+                a += num;
+                Debug.Log(a);
+            };
+            nc(5);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            nc = new NumberChanger(AddNum);
+            nc(5);
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            nc = new NumberChanger(MultNum);
+            nc(5);
+        }
+    }
+}
---
+++ Assets/Scripts/Test4.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test4 : MonoBehaviour
+{
+    public float radius;
+    public Color color;
+}
